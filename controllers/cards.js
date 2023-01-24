68f2263c25e2ebe3feb43d3ade242f6297cc01ec
Elena Fedotova <e-a-fedotova@yandex.ru>
2023-01-25 01:43:01 +0300

    поправить дизлайки и delete

--- controllers/cards.js
+++ controllers/cards.js
@@ -32,14 +32,14 @@ const deleteCard = async (req, res) => {
     return res.status(200).send(card);
   } catch (err) {
     const { cardId } = req.params;
+    if (err.message === 'not found') {
+      return res.status(404).send({ message: 'Карточка не найдена' });
+    }
     if (!Card.findById(cardId).owner.equals(req.user._id)) {
       return res.status(403).send({ message: 'Попытка удалить чужую карточку' });
     }
     if (err.name === 'CastError') {
       return res.status(400).send({ message: 'Ошибка валидации ID', ...err });
-    }
-    if (err.message === 'not found') {
-      return res.status(404).send({ message: 'Карточка не найдена' });
     }
     return res.status(500).send({ message: 'Error' });
   }
@@ -47,8 +47,6 @@ const deleteCard = async (req, res) => {
 
 const likeCard = async (req, res) => {
   try {
-    console.log(`CARD ID ${req.params.cardId}`);
-    console.log(`USER ID ${req.user._id}`);
     const card = await Card.findByIdAndUpdate(
       req.params.cardId,
       { $addToSet: { likes: req.user._id } }, // добавить _id в массив, если его там нет
@@ -58,7 +56,7 @@ const likeCard = async (req, res) => {
     return res.status(200).send(card);
   } catch (err) {
     if (err.message === 'not found') {
-      return res.status(404).send({ message: 'Карточка не найден' });
+      return res.status(404).send({ message: 'Карточка не найдена' });
     }
     if (err.name === 'CastError') {
       return res.status(400).send({ message: 'Ошибка валидации ID', ...err });
@@ -73,12 +71,12 @@ const dislikeCard = async (req, res) => {
       req.params.cardId,
       { $pull: { likes: req.user._id } }, // убрать _id из массива
       { new: true },
-    );
+    ).populate('likes');
     if (!card) throw new Error('not found');
-    return res.status(200).send(Card.findById(req.params.cardId));
+    return res.status(200).send(card);
   } catch (err) {
     if (err.message === 'not found') {
-      return res.status(404).send({ message: 'Карточка не найден' });
+      return res.status(404).send({ message: 'Карточка не найдена' });
     }
     if (err.name === 'CastError') {
       return res.status(400).send({ message: 'Ошибка валидации ID', ...err });
